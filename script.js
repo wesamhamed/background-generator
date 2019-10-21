80f46fb18f3254cc0df6763b7a38f1beeaa065d4
wesamhamed <w.m.h.h2022@gmail.com>
2019-10-21 12:59:30 +0300

    changing script file

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// var button = document.getElementById("enter");
-// var input = document.getElementById("userinput");
-// var ul = document.querySelector("ul");
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
-// function inputLength() {
-// 	return input.value.length;
-// }
+function inputLength() {
+	return input.value.length;
+}
 
-// function createListElement() {
-// 	var li = document.createElement("li");
-// 	li.appendChild(document.createTextNode(input.value));
-// 	ul.appendChild(li);
-// 	input.value = "";
-// }
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
 
-// function addListAfterClick() {
-// 	if (inputLength() > 0) {
-// 		createListElement();
-// 	}
-// }
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
 
-// function addListAfterKeypress(event) {
-// 	if (inputLength() > 0 && event.keyCode === 13) {
-// 		createListElement();
-// 	}
-// }
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
 
-// button.addEventListener("click", addListAfterClick);
+button.addEventListener("click", addListAfterClick);
 
-// input.addEventListener("keypress", addListAfterKeypress);
+input.addEventListener("keypress", addListAfterKeypress);
 // const first= ()=>{
 // 	let greeting ="hi";
 // 	const second =()=>{
